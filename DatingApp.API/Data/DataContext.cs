6fc5eb792dd3d2a8b337b285ff1676fde0d689a1
ahmed <ahmed_20171531@icloud.com>
2020-08-19 05:57:31 +0300

    commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
